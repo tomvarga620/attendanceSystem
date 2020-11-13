60f949d5e3c6b535f0e047717b0c2f110c5000db
Tomas Varga <tomas.varga@akademiasovy.sk>
2020-11-14 00:27:57 +0100

    fixes

--- web/source/src/app/topbar/topbar.component.ts
+++ web/source/src/app/topbar/topbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
-import { Select, Store } from '@ngxs/store';
+import { Actions, ofActionSuccessful, Select, Store } from '@ngxs/store';
 import { Logout } from 'src/store/auth/auth.actions';
 import { AuthState } from 'src/store/auth/auth.state';
 import { Observable } from 'rxjs';
@@ -18,18 +18,19 @@ export class TopbarComponent implements OnInit {
 
   @Select(AuthState.isAdmin) isAdmin$: Observable<boolean>;
 
-  constructor(private store: Store, private router: Router) {}
+  constructor(private store: Store, private router: Router, private actions$: Actions) {
+    this.actions$.pipe(ofActionSuccessful(Logout)).subscribe(() => {
+        this.router.navigate(['login']);
+    });
+  }
 
   ngOnInit() {
-    this.store.select(state => !!state.userAuth.token).subscribe( (value) => {
+    this.store.select(state => !!state.userAuth.token).subscribe((value) => {
       this.isLogged = value;
     });
   }
 
   logoutUser(){
-    this.store.dispatch(new Logout())
-    .subscribe( () => {
-      this.router.navigate(['/login']);
-    });
+    this.store.dispatch(new Logout());
   }
 }
