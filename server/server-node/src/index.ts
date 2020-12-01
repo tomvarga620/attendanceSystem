b437860219abb349264dbfca4e33a780808b5ae1
Tomas Varga <tomas.varga@akademiasovy.sk>
2020-12-01 23:46:03 +0100

    fixes to logger

--- server/server-node/src/index.ts
+++ server/server-node/src/index.ts
@@ -6,12 +6,9 @@ import cors from 'cors';
 import router from './routes/routes';
 import "reflect-metadata";
 import { createConnection } from 'typeorm';
-import {logger} from './middleware/customLogger'
+import { errorLogger, requestLogger } from "./middleware/customLogger";
 
 dotenv.config({ path: resolve(__dirname, ".env") });
-const moment = require('moment');
-var winston = require('winston'),
-    expressWinston = require('express-winston');
 
 createConnection().then(async connection => {
 
@@ -20,34 +17,9 @@ createConnection().then(async connection => {
     app.use(express.urlencoded({extended: true})); 
     app.use(express.json());
     app.use(cors(options));
-
-    app.use(expressWinston.logger({
-        transports: [
-          new winston.transports.File({ 
-              filename: 'combined.log' ,
-              json: true,
-          }),
-          new winston.transports.Console({
-            json: true,
-            colorize: true
-          })
-        ]
-    }));
-
+    app.use(requestLogger);
     app.use('/',router);
-
-    app.use(expressWinston.errorLogger({
-        transports: [
-          new winston.transports.File({ 
-              filename: 'error.log' ,
-              json: true,
-          }),
-          new winston.transports.Console({
-              json: true,
-              colorize: true
-          })
-        ]
-    }));
+    app.use(errorLogger);
 
     app.listen(process.env.PORT); 
 });
--- server/server-node/src/middleware/customLogger.ts
+++ server/server-node/src/middleware/customLogger.ts
@@ -1,49 +1,29 @@
-/* import express, { NextFunction } from 'express'
 
-export const logger = (request: express.Request, response: express.Response, next: NextFunction) => {
-    console.log(`${request.method} ${request.path} ${response}`);
-} */
+const winston = require('winston'),
+expressWinston = require('express-winston');
 
-import { NextFunction } from 'express';
-import {
-    createLogger,
-    Logger as WinstonLogger,
-    transports,
-    format,
-} from 'winston';
-
-const winston = require('winston');
-
-const logConfiguration = {
-    'transports': [
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'combined.log' }),
-    ]
-};
-
-export const logger = winston.createLogger({
+export const requestLogger = expressWinston.logger({
     transports: [
-        new (winston.transports.File)({
-            filename: 'error.log',
-            level: 'error'
-        }),
-        new (winston.transports.File)({
-            filename: 'combined.log',
-            level: 'info'
-        })
+      new winston.transports.File({ 
+          filename: 'combined.log' ,
+          json: true,
+      }),
+      new winston.transports.Console({
+        json: true,
+        colorize: true
+      })
     ]
 });
 
-// const logger = winston.createLogger(logConfiguration);
-
-export const logRequest = (request: Request, response: Response, next: NextFunction) => {
-    logger.info(`${request.url}, ${response.status}`);
-    next();
-}
-
-export const logError = (err: any,req: Request, res: Response, next: NextFunction) => { 
-    logger.error(err)
-    next();
-}
-
-// module.exports = logger;
+export const errorLogger = expressWinston.errorLogger({
+    transports: [
+        new winston.transports.File({ 
+            filename: 'error.log' ,
+            json: true,
+        }),
+        new winston.transports.Console({
+            json: true,
+            colorize: true
+        })
+    ]
+});
