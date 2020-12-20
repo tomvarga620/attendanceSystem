7c0808c80ad617ad46419dc036488f56e3fc1869
tomas varga <tomas.varga@kestramedical.com>
2020-12-20 23:28:39 +0100

    added new material component in shared module

--- web/source/src/modules/shared/shared.module.ts
+++ web/source/src/modules/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GenericTableComponent } from './generic-table/generic-table.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [GenericTableComponent],
   imports: [
     CommonModule
   ]
