a4aa77e32dfae6f1d0a622946cb26b469faf0bd2
tomas varga <tomas.varga@kestramedical.com>
2020-12-21 23:38:47 +0100

    generated component for generic dialog

--- web/source/src/modules/shared/shared.module.ts
+++ web/source/src/modules/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GenericTableComponent } from './generic-table/generic-table.component';
+import { GenericDialogComponent } from './generic-dialog/generic-dialog.component';
 
 
 @NgModule({
-  declarations: [GenericTableComponent],
+  declarations: [GenericTableComponent, GenericDialogComponent],
   imports: [
     CommonModule
   ]
