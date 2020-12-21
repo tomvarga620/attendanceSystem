3f2e95b794745659bc5dc8da80b0c0bca1cd4e97
tomas varga <tomas.varga@kestramedical.com>
2020-12-21 23:51:34 +0100

    export table in shared module

--- web/source/src/modules/shared/shared.module.ts
+++ web/source/src/modules/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GenericTableComponent } from './generic-table/generic-table.component';
 
-
 @NgModule({
   declarations: [GenericTableComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    GenericTableComponent
   ]
 })
 export class SharedModule { }
