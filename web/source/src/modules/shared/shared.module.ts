b92d9c0e2f992a5583afdd38ea6b1173e88477fb
tomas varga <tomas.varga@kestramedical.com>
2020-12-21 23:35:09 +0100

    fix

--- web/source/src/modules/shared/shared.module.ts
+++ web/source/src/modules/shared/shared.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { GenericTableComponent } from './generic-table/generic-table.component';
 
 
-
 @NgModule({
   declarations: [GenericTableComponent],
   imports: [
