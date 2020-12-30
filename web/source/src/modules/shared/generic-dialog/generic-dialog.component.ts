142495f6df6763cda70b06028d98ead0afe2b925
tomas varga <tomas.varga@kestramedical.com>
2020-12-30 01:15:54 +0100

    added basic html and material dialog inject in constructor in GenericDialogComponent

--- web/source/src/modules/shared/generic-dialog/generic-dialog.component.ts
+++ web/source/src/modules/shared/generic-dialog/generic-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-generic-dialog',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenericDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {
+    cancelText: string,
+    confirmText: string,
+    message: string,
+    title: string
+  }, private mdDialogRef: MatDialogRef<GenericDialogComponent>){}
 
   ngOnInit(): void {
   }
