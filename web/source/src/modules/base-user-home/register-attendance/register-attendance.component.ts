f77b9fb11c9dc8c17037c0965ee5d7e08d7c5a74
Tom Varga <tomvarga@MacBook-Pro-uzivatela-Tom.local>
2021-01-10 19:28:16 +0100

    fixes,removed redundant modules,added base components for attendance

---
+++ web/source/src/modules/base-user-home/register-attendance/register-attendance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-attendance',
+  templateUrl: './register-attendance.component.html',
+  styleUrls: ['./register-attendance.component.css']
+})
+export class RegisterAttendanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
